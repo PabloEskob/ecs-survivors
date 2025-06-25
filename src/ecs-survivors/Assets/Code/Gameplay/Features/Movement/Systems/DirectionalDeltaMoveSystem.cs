876a9833336f8ecb99983455e081e90d96d53b14
Pavel Salogub <pavelae27@mail.ru>
2025-06-26 01:28:57 +0500

    Init
    Init

---
+++ src/ecs-survivors/Assets/Code/Gameplay/Features/Movement/Systems/DirectionalDeltaMoveSystem.cs
@@ -0,0 +1,29 @@
+﻿using Code.Gameplay.Common.Time;
+using Entitas;
+
+namespace Code.Gameplay.Features.Movement.Systems
+{
+    public class DirectionalDeltaMoveSystem : IExecuteSystem
+    {
+        private readonly IGroup<GameEntity> _movers;
+        private readonly ITimeService _timer;
+
+        public DirectionalDeltaMoveSystem(GameContext gameContext, ITimeService timerService)
+        {
+            _timer = timerService;
+            _movers = gameContext.GetGroup(GameMatcher
+                .AllOf(
+                GameMatcher.WorldPosition,
+                GameMatcher.Speed,
+                GameMatcher.Moving));
+        }
+
+        public void Execute()
+        {
+            foreach (var mover in _movers)
+            {
+                mover.ReplaceWorldPosition(mover.WorldPosition + mover.Direction * mover.Speed * _timer.DeltaTime);
+            }
+        }
+    }
+}
